7f28d1b012edd8a24f3d0368550b1501873fcfb8
aereal <aereal@aereal.org>
2020-10-29 19:16:06 +0900

    test: add tests for imported table

--- test/timestream/table.test.ts
+++ test/timestream/table.test.ts
@@ -1,6 +1,6 @@
 import { User } from "@aws-cdk/aws-iam";
-import { App, Duration, Stack } from "@aws-cdk/core";
-import { Database, Table } from "../../src/timestream";
+import { App, Construct, Duration, Stack } from "@aws-cdk/core";
+import { Database, ITable, Table } from "../../src/timestream";
 import "@aws-cdk/assert/jest";
 
 describe("Table", () => {
@@ -46,52 +46,77 @@ describe("Table", () => {
   });
 
   describe("IAM actions", () => {
-    test("grantRead", () => {
-      const stack = new Stack();
-      const user = new User(stack, "User");
-      const db = new Database(stack, "DB");
-      const table = db.addTable("Table");
-      table.grantRead(user);
-
-      expect(stack).toHaveResource("AWS::IAM::Policy", {
-        PolicyDocument: {
-          Version: "2012-10-17",
-          Statement: [
-            {
-              Action: [
-                "timestream:Select",
-                "timestream:ListMeasures",
-                "timestream:DescribeTable",
-              ],
-              Effect: "Allow",
-              Resource: { "Fn::GetAtt": ["DBTableB8E07EFE", "Arn"] },
-            },
-          ],
+    test.each([
+      [
+        "grantRead",
+        "built resource",
+        (scope: Construct): ITable => {
+          const db = new Database(scope, "DB");
+          return db.addTable("Table");
         },
-        PolicyName: "UserDefaultPolicy1F97781E",
-        Users: [
-          {
-            Ref: "User00B015A1",
-          },
+        [
+          "timestream:Select",
+          "timestream:ListMeasures",
+          "timestream:DescribeTable",
         ],
-      });
-    });
-
-    test("grantWrite", () => {
-      const stack = new Stack();
+        {
+          "Fn::GetAtt": ["DBTableB8E07EFE", "Arn"],
+        },
+      ],
+      [
+        "grantRead",
+        "imported resource",
+        (scope: Construct): ITable => {
+          const app = (scope.node.scope as unknown) as App;
+          const stack = new Stack(app, "src-stack");
+          const db = new Database(stack, "DB");
+          const srcTable = db.addTable("Table");
+          return Table.fromTableArn(scope, "Table", srcTable.tableArn);
+        },
+        [
+          "timestream:Select",
+          "timestream:ListMeasures",
+          "timestream:DescribeTable",
+        ],
+        {
+          "Fn::ImportValue":
+            "src-stack:ExportsOutputFnGetAttDBTableB8E07EFEArn3442B74A",
+        },
+      ],
+      [
+        "grantWrite",
+        "built resource",
+        (scope: Construct): ITable => {
+          const db = new Database(scope, "DB");
+          return db.addTable("Table");
+        },
+        "timestream:WriteRecords",
+        {
+          "Fn::GetAtt": ["DBTableB8E07EFE", "Arn"],
+        },
+      ],
+    ])("%s/%s", (method, kind, buildTable, actions, targetResource) => {
+      const app = new App();
+      const stack = new Stack(app, "stack-1");
       const user = new User(stack, "User");
-      const db = new Database(stack, "DB");
-      const table = db.addTable("Table");
-      table.grantWrite(user);
+      const table = buildTable(stack);
+      switch (method) {
+        case "grantRead":
+          table.grantRead(user);
+          break;
+        case "grantWrite":
+          table.grantWrite(user);
+          break;
+      }
 
       expect(stack).toHaveResource("AWS::IAM::Policy", {
         PolicyDocument: {
           Version: "2012-10-17",
           Statement: [
             {
-              Action: "timestream:WriteRecords",
+              Action: actions,
               Effect: "Allow",
-              Resource: { "Fn::GetAtt": ["DBTableB8E07EFE", "Arn"] },
+              Resource: targetResource,
             },
           ],
         },
